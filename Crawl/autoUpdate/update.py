640ab5a1b21a44c470871e97dc9441918f02d988
quanth02 <anhquankaka113@gmail.com>
2024-07-07 06:21:02 +0700

    update

--- Crawl/autoUpdate/update.py
+++ Crawl/autoUpdate/update.py
@@ -1,17 +1,27 @@
 import mysql.connector
 
-def execute_sql_file(filename, connection):
+def execute_sql_file(filename, connection, chunk_size=1000):
     with open(filename, 'r', encoding='utf-8') as file:
-        sql_script = file.read()
+        sql_commands = []
+        for line in file:
+            sql_commands.append(line.strip())
+            if len(sql_commands) >= chunk_size:
+                execute_sql_commands(sql_commands, connection)
+                sql_commands = []
+        
+        if sql_commands:
+            execute_sql_commands(sql_commands, connection)
 
+def execute_sql_commands(sql_commands, connection):
     cursor = connection.cursor()
-    for result in cursor.execute(sql_script, multi=True):
-        pass
+    for sql_command in sql_commands:
+        if sql_command.strip():
+            cursor.execute(sql_command)
     connection.commit()
+    cursor.close()
 
-conn = mysql.connector.connect(host = "localhost", password = "123456", user = "root")
-mycursor = conn.cursor()
-
-execute_sql_file('./update.sql', conn)
-
-conn.close()
+conn = mysql.connector.connect(host="localhost", password="123456", user="root")
+try:
+    execute_sql_file('./update.sql', conn)
+finally:
+    conn.close()
